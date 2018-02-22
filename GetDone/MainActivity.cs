e35f82ee373c1ac3c8c380becf617ec88d8c2e0f
prashant-bhandari <bhandariprashant94@gmail.com>
2018-02-22 11:21:10 +1000

    3 screens- Login, Signup and SingIn
    Next Step- Navigate through buttons

---
+++ GetDone/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+
+
+
+
+
+namespace GetDone
+{
+    
+    [Activity(Label = "GetDone", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        //int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            SetContentView(Resource.Layout.Login);
+
+           
+            Button button = FindViewById<Button>(Resource.Id.Signin);
+
+           // public void Sign_in_details()
+
+            //button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
